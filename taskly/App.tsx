cdebfe0dd4a2031b2abf63f403b8ea102556d458
Harshil Patel <harshilp046@gmail.com>
2024-11-24 23:50:07 +0530

    Added a list item, and a delete button.

--- taskly/App.tsx
+++ taskly/App.tsx
@@ -1,21 +1,37 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, TouchableOpacity, Alert } from "react-native";
+import { styles } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to DELETE this?",
+      "It will be GONE for good.",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok, Deleting.."),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          onPress: () => console.log("Then why did u click this BITCH.."),
+          style: "cancel",
+        },
+      ],
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Is this thing working?</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Oysters</Text>
+        <TouchableOpacity
+          onPress={handleDelete}
+          activeOpacity={0.8}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
